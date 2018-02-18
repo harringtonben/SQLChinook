180a2babdbf06a587dea4af2983bcee83c7300d0
Ben Harrington <harringtonben@icloud.com>
2018-02-18 10:19:10 -0600

    finished 4 of the query problems

--- SQLChinook.sql
+++ SQLChinook.sql
@@ -9,3 +9,13 @@ select distinct country from Customer
 select firstname + ' ' + lastname as Name, customerid, country from Customer
 where country != 'USA'
 
+-- Provide a query only showing the Customers from Brazil.
+select * from customer
+where country = 'Brazil'
+
+/*Provide a query showing the Invoices of customers who are from Brazil. 
+The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country. */
+select c.firstname + ' ' + c.lastname as Name, i.invoiceid, i.invoicedate, i.billingcountry from customer c
+join invoice i on i.CustomerId = c.CustomerId
+where c.country = 'Brazil'
+
